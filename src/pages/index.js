cf5e69c77015373f4f586c40b96779519c92a0da
patricksoga <psoga@nd.edu>
2021-02-26 10:43:16 -0800

    helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import Home from './home/Home';
 import 'katex/dist/katex.min.css';
+import { Helmet } from "react-helmet";
 
 export default function HomeIndex() {
   return (
-    <Home/>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>AJB117</title>
+      </Helmet>
+      <Home/>
+    </>
   )
 }
