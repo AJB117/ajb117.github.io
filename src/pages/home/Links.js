8f1e937ed7acb96d5a4b627eba50a30dd97e7db4
AJB117 <psoga117@gmail.com>
2021-03-05 16:43:07 -0800

    added cv

--- src/pages/home/Links.js
+++ src/pages/home/Links.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby';
 const Links = () => {
   return (
     <div className="links">
-      <a href="">CV &#8226; </a>
+      <a href="../../../static/cv.pdf" download>CV &#8226; </a>
       <Link to="/blog">
         Blog &#8226;
       </Link>
