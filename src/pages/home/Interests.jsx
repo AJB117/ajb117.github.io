fa27e21e19f3b872d1f4d802fb3155e55f0d7cd2
psoga <psoga117@gmail.com>
2023-09-02 16:50:32 -0700

    reduce links

--- src/pages/home/Interests.jsx
+++ src/pages/home/Interests.jsx
@@ -7,24 +7,17 @@ const Interests = () => (
   <section className="interests">
     <p>
       I'm a first-year PhD student at the{" "}
-      <Href href="https://www.virginia.edu">University of Virginia's</Href>{" "}
-      <Href href="https://engineering.virginia.edu/departments/computer-science/">
-        CS department
-      </Href>{" "}
-      currently advised by{" "}
+      <Href href="https://www.virginia.edu">University of Virginia's</Href> CS
+      department currently advised by{" "}
       <Href href="https://jundongli.github.io">Prof. Jundong Li</Href>. My
       interests are broadly in machine learning on graphs and its applications.
-      I did my undergrad at the{" "}
+      Previously, I did my undergrad at the{" "}
       <Href href="https://nd.edu">University of Notre Dame </Href>
-      where I received degrees in <Href href="https://cse.nd.edu/">
-        CS
-      </Href> and{" "}
-      <Href href="https://philosophy.nd.edu/majors-minors/philosophy-science-and-mathematics/major-concentration/">
-        philosophy of math/logic
+      where I studied CS and philosophy of math/logic and worked under
+      professors <Href href="https://niteshchawla.nd.edu/">
+        Nitesh Chawla
       </Href>{" "}
-      and worked under professors{" "}
-      <Href href="https://niteshchawla.nd.edu/">Nitesh Chawla</Href> and{" "}
-      <Href href="https://www3.nd.edu/~dchiang/">David Chiang</Href>.
+      and <Href href="https://www3.nd.edu/~dchiang/">David Chiang</Href>.
     </p>
   </section>
 )
